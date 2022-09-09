9df37e62f6e7068d0d04d6b2748488c82c4fe62c
debnix <daquipe5@gmail.com>
2022-09-08 20:51:09 -0500

    introduction-hibernate: add SessionFactory and open session into App.java

--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -1,13 +1,43 @@
 package com.hibernate.introduction;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.introduction.modelo.Mascota;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // Crear objeto fabricante de sesiones
+        SessionFactory factory = new Configuration()
+                .configure("cfg.xml")
+                .addAnnotatedClass(Mascota.class)
+                .buildSessionFactory();
+
+        // Abrir sesión
+        Session session = factory.openSession();
+
+        // Preparar la sesión para realizar transacciones
+        session.beginTransaction();
+
+        // Generar transacciones...
+        try {
+            // CRUD
+
+            // Create
+            Mascota mascota = new Mascota("Niño", "Quintero", "Perro", "Chandinez", 6, "Ninguna");
+            session.persist(mascota);
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Cerrar sesión
+        session.close();
     }
 }
