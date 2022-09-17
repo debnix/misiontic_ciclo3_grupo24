316d00f21c145ee6cf91f414ae3ca73f1c210835
debnix <daquipe5@gmail.com>
2022-09-17 14:25:15 -0500

    mini-veterinaria: add update method into form.js

--- mini-veterinaria/scripts/form.js
+++ mini-veterinaria/scripts/form.js
@@ -1,4 +1,8 @@
 const URL_API = "http://localhost:8080/mascotas"
+let update_data = {
+  update: false,
+  id: null
+}
 
 function get_data_form (evt) {
   // Indicar por medio del evento que no recargue página
@@ -12,8 +16,29 @@ function get_data_form (evt) {
     edad: form.edad.value,
     observacion: form.observacion.value
   }
-  create(mascota)
+
+  if (update_data.update) {
+    mascota.id = update_data.id
+    update(mascota)
+  } else {
+    create(mascota)
+  }
+
   clear(form)
+}
+
+async function update (mascota) {
+  // Enviar petición
+  const resp = await fetch(URL_API, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(mascota)
+  })
+  const text = await resp.text()
+  alert(text)
+  window.location.href = "index.html"
 }
 
 async function create (mascota) {
@@ -56,6 +81,10 @@ function get_params () {
     const mascota = JSON.parse(param_mascota)
     const form = document.getElementById("form")
     set_form(form, mascota)
+    update_data.update = true
+    update_data.id = mascota.id
+    document.getElementById("btn-form").innerText = "Actualizar"
+    document.getElementById("link-create").innerText = "Actualizar mascota"
   }
 }
 
