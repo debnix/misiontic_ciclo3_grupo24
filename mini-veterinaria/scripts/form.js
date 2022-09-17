2f43a167dd74054814055cddd4fe2e910d7e85fe
debnix <daquipe5@gmail.com>
2022-09-16 19:32:21 -0500

    mini-veterinaria: add create into form.js

---
+++ mini-veterinaria/scripts/form.js
@@ -0,0 +1,30 @@
+const URL_API = "http://localhost:8080/mascotas"
+
+function get_data_form (evt) {
+  // Indicar por medio del evento que no recargue página
+  evt.preventDefault()
+  const form = evt.target
+  const mascota = {
+    nombre: form.nombre.value,
+    apellido: form.apellido.value,
+    tipo_mascota: form.tipo_mascota.value,
+    raza: form.raza.value,
+    edad: form.edad.value,
+    observacion: form.observacion.value
+  }
+  create(mascota)
+}
+
+async function create (mascota) {
+  // Enviar petición
+  const resp = await fetch(URL_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(mascota)
+  })
+
+  const text = await resp.text()
+  alert(text)
+}
